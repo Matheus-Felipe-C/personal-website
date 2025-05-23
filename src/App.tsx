79b41bdf81abfcb40a28bfd7ecfd0570fe75f32a
Matheus Felipe <theusf40@hotmail.com>
2025-05-22 21:53:58 -0300

    Add profile image and fixed styling issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import './App.css'
 import githubLogo from './assets/github.svg';
 import linkedinLogo from './assets/linkedin.svg';
-import emailLogo from './assets/envelope.svg';
 import substackLogo from './assets/substack.svg';
+import myselfPic from './assets/myself.jpg';
 
 function App() {
 
@@ -23,20 +23,25 @@ function App() {
 
         <main>
             <section className="hero-section">
-                <h1>Hi, I'm Matheus Felipe</h1>
-                <h2>Back End Developer</h2>
-                <p className='initial-pitch'>
-                    A passionate developer, with knowledge in React, PHP, and TypeScript. Currently pursuing a Bachelor's in Information Systems at IFBA.
-                </p>
-                <div className="cta-btn">
-                    <button className='primary-btn'>View my work</button>
-                    <button className='primary-btn'>Resume</button>
+                <div className="hero-summary">
+                    <div className="hero-summary-text">
+                        <h1>Hi, I'm Matheus Felipe</h1>
+                        <h2>Back End Developer</h2>
+                        <p className='initial-pitch'>
+                            A passionate developer, with knowledge in React, PHP, and TypeScript. Currently pursuing a Bachelor's in Information Systems at IFBA.
+                        </p>
+                        </div>
+                    <div className="cta-btn">
+                        <button className='primary-btn'>View my work</button>
+                        <button className='primary-btn'>Resume</button>
+                    </div>
+                    <div className="hero-socials">
+                        <a className='social-btn' href="https://github.com/Matheus-Felipe-C" target='_blank'><img src={githubLogo} alt="GitHub Logo" /></a>
+                        <a className='social-btn' href="https://www.linkedin.com/in/matheus-felipe-dev/" target='_blank'><img src={linkedinLogo} alt="LinkedIn logo" /></a>
+                        <a className='social-btn' href="https://www.productiveprocrastination.dev/" target='_blank'><img src={substackLogo} alt="substack logo" /></a>
+                    </div>
                 </div>
-                <div className="hero-socials">
-                    <a className='social-btn' href="https://github.com/Matheus-Felipe-C" target='_blank'><img src={githubLogo} alt="GitHub Logo" /></a>
-                    <a className='social-btn' href="https://www.linkedin.com/in/matheus-felipe-dev/" target='_blank'><img src={linkedinLogo} alt="LinkedIn logo" /></a>
-                    <a className='social-btn' href="https://www.productiveprocrastination.dev/" target='_blank'><img src={substackLogo} alt="substack logo" /></a>
-                </div>
+                <img src={myselfPic} alt="A picture of myself, using a black shirt, with short hair and glasses" className='profile-picture'/>
             </section>
         </main>
     </>
