7244ff62f259b9b619690e2842a3d92ef454bf02
Matheus Felipe <theusf40@hotmail.com>
2025-05-23 21:50:03 -0300

    Add header with mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,49 @@
 import './App.css'
 import '@mantine/core/styles.css';
 
-import { AppShell, Flex, Burger, Button} from '@mantine/core';
+import { AppShell, Flex, Burger, ActionIcon, useComputedColorScheme, useMantineColorScheme } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
-
-import githubLogo from './assets/github.svg';
-import linkedinLogo from './assets/linkedin.svg';
-import substackLogo from './assets/substack.svg';
-import myselfPic from './assets/myself.jpg';
+import { IconSun } from '@tabler/icons-react';
 
 function App() {
     
     const [opened, {toggle}] = useDisclosure();
+    const { colorScheme, setColorScheme } = useMantineColorScheme();
+    const computedColorScheme = useComputedColorScheme('light');
+
+    const toggleColorScheme = () => {
+        setColorScheme(computedColorScheme === 'dark' ? 'light': 'dark');
+    }
 
     return (
-            <AppShell
-                header={{ height: 60 }}
-                navbar={{ width: 300, breakpoint: 'sm', collapsed: {mobile: !opened} }}
-                padding='md'
-            >
-                <AppShell.Header>
-                    <Flex justify='space-between' align='center' style={{ padding: '10px 20px' }}>
+        <AppShell
+            header={{ height: 60 }}
+            padding='md'
+        >
+            <AppShell.Header>
+                <div style={{ margin: '0 auto', padding: '0 20px', height: '100%' }}>
+                    <Flex justify='space-between' align='center' style={{ height: '100%'}}>
                         <Burger opened={opened} onClick={toggle} hiddenFrom='sm' size='sm'/>
-                        <div>Quirky Mantine Tutorials</div>
-                        <Button size='sm' variant='link'><img src={githubLogo} alt="" /></Button>
-                    </Flex>
-                </AppShell.Header>
-               <AppShell.Main>
-                </AppShell.Main>
+                        <a href="">Matheus</a>
+                        <Flex gap='lg'>
+                            <a href="">Home</a>
+                            <a href="">About</a>
+                            <a href="">Experience</a>
+                            <a href="">Contacts</a>
+                        </Flex>
+                        <ActionIcon variant='subtle' onClick={toggleColorScheme}>
+                            <IconSun></IconSun>
+                        </ActionIcon>
+                   </Flex>
+                </div>
+           </AppShell.Header>
+           <AppShell.Main>
+            </AppShell.Main>
 
-                <AppShell.Footer>
-                    Feito por Matheus Felipe
-                </AppShell.Footer>
-            </AppShell>
-        
+            <AppShell.Footer>
+                Made by Matheus Felipe
+            </AppShell.Footer>
+        </AppShell>
     )
 }
 export default App
